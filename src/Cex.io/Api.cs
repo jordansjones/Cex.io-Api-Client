f9b4d6c6f6fa08ef23186beb085260157146bcd6
Jordan S. Jones <jordansjones@gmail.com>
2014-01-13 21:17:03 -0700

    Cleaned up API class a bit.

--- src/Cex.io/Api.cs
+++ src/Cex.io/Api.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,6 +9,10 @@ namespace Nextmethod.Cex
 {
     public sealed class Api
     {
+
+        #region Static Helpers
+
+        private static readonly Func<IEnumerable<KeyValuePair<string, string>>> EmptyRequestParams = () => Enumerable.Empty<KeyValuePair<string, string>>();
 
         static Api()
         {
@@ -24,10 +26,7 @@ namespace Nextmethod.Cex
             }
         }
 
-
-        public TimeSpan? Timeout { get; set; }
-
-        private ApiCredentials Credentials { get; set; }
+        #endregion
 
 
         public Api(string username, string apiKey, string apiSecret)
@@ -38,17 +37,28 @@ namespace Nextmethod.Cex
             Credentials = credentials;
         }
 
+        public TimeSpan? Timeout { get; set; }
+
+        public ApiCredentials Credentials { get; private set; }
+
 
         public async Task<Ticker> Ticker(SymbolPair pair, CancellationTokenSource tokenSource = null)
         {
             const string basePath = "/ticker";
             var path = string.Format("{0}/{1}/{2}", basePath, pair.From, pair.To);
 
-            return await GetServiceCall(
-                path,
-                Cex.Ticker.FromDynamic,
-                tokenSource
-                );
+            try
+            {
+                return await this.GetFromService(
+                    path,
+                    Cex.Ticker.FromDynamic,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<OrderBook> OrderBook(SymbolPair pair, CancellationTokenSource tokenSource = null)
@@ -56,11 +66,18 @@ namespace Nextmethod.Cex
             const string basePath = "/order_book";
             var path = string.Format("{0}/{1}/{2}", basePath, pair.From, pair.To);
 
-            return await GetServiceCall(
-                path,
-                Cex.OrderBook.FromDynamic,
-                tokenSource
-                );
+            try
+            {
+                return await this.GetFromService(
+                    path,
+                    Cex.OrderBook.FromDynamic,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<IEnumerable<Trade>> TradeHistory(SymbolPair pair, CancellationTokenSource tokenSource = null)
@@ -68,23 +85,37 @@ namespace Nextmethod.Cex
             const string basePath = "/trade_history";
             var path = string.Format("{0}/{1}/{2}", basePath, pair.From, pair.To);
 
-            return await GetServiceCall(
-                path,
-                Trade.FromDynamic,
-                tokenSource
-                );
+            try
+            {
+                return await this.GetFromService(
+                    path,
+                    Trade.FromDynamic,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<Balance> AccountBalance(CancellationTokenSource tokenSource = null)
         {
             const string basePath = "/balance/";
 
-            return await PostServiceCall(
-                basePath,
-                () => EmptyRequestParams,
-                Balance.FromDynamic,
-                tokenSource
-                );
+            try
+            {
+                return await this.PostToService(
+                    basePath,
+                    EmptyRequestParams,
+                    Balance.FromDynamic,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<IEnumerable<OpenOrder>> OpenOrders(SymbolPair pair, CancellationTokenSource tokenSource = null)
@@ -92,18 +123,25 @@ namespace Nextmethod.Cex
             const string basePath = "/open_orders";
             var path = string.Format("{0}/{1}/{2}", basePath, pair.From, pair.To);
 
-            return await PostServiceCall(
-                path,
-                () => EmptyRequestParams,
-                x =>
-                {
-                    var ja = x as JsonArray;
-                    return ja == null 
-                        ? Enumerable.Empty<OpenOrder>() 
-                        : ja.Select(OpenOrder.FromDynamic).ToArray();
-                },
-                tokenSource
-                );
+            try
+            {
+                return await this.PostToService(
+                    path,
+                    EmptyRequestParams,
+                    x =>
+                    {
+                        var ja = x as JsonArray;
+                        return ja == null
+                            ? Enumerable.Empty<OpenOrder>()
+                            : ja.Select(OpenOrder.FromDynamic).ToArray();
+                    },
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<OpenOrder> PlaceOrder(SymbolPair pair, Order order, CancellationTokenSource tokenSource = null)
@@ -111,92 +149,44 @@ namespace Nextmethod.Cex
             const string basePath = "/place_order";
             var path = string.Format("{0}/{1}/{2}", basePath, pair.From, pair.To);
 
-            return await PostServiceCall(
-                path,
-                () => new[]
-                {
-                    new KeyValuePair<string, string>("type", order.Type == OrderType.Sell ? "sell" : "buy"),
-                    new KeyValuePair<string, string>("price", order.Price.ToString()),
-                    new KeyValuePair<string, string>("amount", order.Amount.ToString())
-                },
-                OpenOrder.FromDynamic,
-                tokenSource
-                );
+            try
+            {
+                return await this.PostToService(
+                    path,
+                    () => new[]
+                    {
+                        this.NewRequestParam("type", order.Type == OrderType.Sell ? "sell" : "buy"),
+                        this.NewRequestParam("price", order.Price.ToString()),
+                        this.NewRequestParam("amount", order.Amount.ToString())
+                    },
+                    OpenOrder.FromDynamic,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
         }
 
         public async Task<bool> CancelOrder(TradeId tradeId, CancellationTokenSource tokenSource = null)
         {
             const string basePath = "/cancel_order/";
-            
 
-            return await PostServiceCall(
-                basePath,
-                () => new[] { new KeyValuePair<string, string>("id", tradeId.ToString()) },
-                x => (bool) x,
-                tokenSource
-                );
-        }
-
-        #region Service Calls and Helpers
-
-
-        [DebuggerHidden]
-        private async Task<T> GetServiceCall<T>(string servicePath, Func<dynamic, T> resultFactory, CancellationTokenSource tokenSource = null)
-        {
-            tokenSource = tokenSource ?? new CancellationTokenSource();
-            var uri = this.GetApiUri(servicePath);
-            using (var client = this.NewHttpClient())
+            try
             {
-                using (var response = await client.GetAsync(uri, tokenSource.Token))
-                {
-                    var body = await response.Content.ReadAsStringAsync();
-                    dynamic json = SimpleJson.DeserializeObject(body);
-
-                    ExceptionOracle.ThrowIfError(response, json);
-                    return resultFactory(json);
-                }
+                return await this.PostToService(
+                    basePath,
+                    () => new[] {this.NewRequestParam("id", tradeId.ToString())},
+                    x => (bool) x,
+                    tokenSource
+                    );
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
             }
         }
-
-        [DebuggerHidden]
-        private async Task<T> PostServiceCall<T>(string servicePath, Func<IEnumerable<KeyValuePair<string, string>>> paramFactory, Func<dynamic, T> resultFactory, CancellationTokenSource tokenSource = null)
-        {
-            tokenSource = tokenSource ?? new CancellationTokenSource();
-
-            long nonce;
-            var signature = Credentials.NewSignature(out nonce);
-            var content = new FormUrlEncodedContent(
-                new[]
-                {
-                    NewRequestParam(Constants.ApiParamKey, Credentials.ApiKey),
-                    NewRequestParam(Constants.ApiParamSignature, signature),
-                    NewRequestParam(Constants.ApiParamNonce, Convert.ToString(nonce))
-                }
-                    .Concat(paramFactory())
-                );
-
-            var uri = this.GetApiUri(servicePath);
-            using (var client = this.NewHttpClient())
-            {
-                using (var response = await client.PostAsync(uri, content, tokenSource.Token))
-                {
-                    var body = await response.Content.ReadAsStringAsync();
-                    dynamic json = SimpleJson.DeserializeObject(body);
-
-                    ExceptionOracle.ThrowIfError(response, json);
-                    return resultFactory(json);
-                }
-            }
-        }
-
-        private static KeyValuePair<string, string> NewRequestParam(string key, string value)
-        {
-            return new KeyValuePair<string, string>(key, value);
-        }
-
-        private static readonly IEnumerable<KeyValuePair<string, string>> EmptyRequestParams = Enumerable.Empty<KeyValuePair<string, string>>();
-
-        #endregion
 
 
     }
---
+++ src/Cex.io/Helpers/ApiHelpers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nextmethod.Cex
+{
+    internal static class ApiHelpers
+    {
+
+        [DebuggerHidden]
+        internal static async Task<T> GetFromService<T>(this Api This, string servicePath, Func<dynamic, T> resultFactory, CancellationTokenSource tokenSource = null)
+        {
+            tokenSource = tokenSource ?? new CancellationTokenSource();
+            var uri = This.GetApiUri(servicePath);
+            using (var client = This.NewHttpClient())
+            {
+                using (var response = await client.GetAsync(uri, tokenSource.Token))
+                {
+                    var body = await response.Content.ReadAsStringAsync();
+                    dynamic json = SimpleJson.DeserializeObject(body);
+
+                    ExceptionOracle.ThrowIfError(response, json);
+                    return resultFactory(json);
+                }
+            }
+        }
+
+        [DebuggerHidden]
+        internal static async Task<T> PostToService<T>(this Api This, string servicePath, Func<IEnumerable<KeyValuePair<string, string>>> paramFactory, Func<dynamic, T> resultFactory, CancellationTokenSource tokenSource = null)
+        {
+            tokenSource = tokenSource ?? new CancellationTokenSource();
+
+            long nonce;
+            var signature = This.Credentials.NewSignature(out nonce);
+            var content = new FormUrlEncodedContent(
+                new[]
+                {
+                    NewRequestParam(This, Constants.ApiParamKey, This.Credentials.ApiKey),
+                    NewRequestParam(This, Constants.ApiParamSignature, signature),
+                    NewRequestParam(This, Constants.ApiParamNonce, Convert.ToString(nonce))
+                }
+                    .Concat(paramFactory())
+                );
+
+            var uri = This.GetApiUri(servicePath);
+            using (var client = This.NewHttpClient())
+            {
+                using (var response = await client.PostAsync(uri, content, tokenSource.Token))
+                {
+                    var body = await response.Content.ReadAsStringAsync();
+                    dynamic json = SimpleJson.DeserializeObject(body);
+
+                    ExceptionOracle.ThrowIfError(response, json);
+                    return resultFactory(json);
+                }
+            }
+        }
+
+        internal static KeyValuePair<string, string> NewRequestParam(this Api This, string key, string value)
+        {
+            return new KeyValuePair<string, string>(key, value);
+        }
+
+    }
+}
